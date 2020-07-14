ecdfb642fb09ffadac1b70cc193f58b0432b0d56
NatalieSokolova <nataliesklv@gmail.com>
2020-07-14 21:07:23 +0000

    added comments for clarification

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,33 +1,31 @@
-
 $(document).ready(function() {
-
-  // selector => #tweet-text is my form textarea
-  // section => new-tweet
-  // button = anonymous (in tweet-form-footer)
-  // counter = counter (in tweet-form-footer)
-  // event is change or blur
-  // use .this, make sure it's capturing the right thing
   const $tweetText = ('#tweet-text');
   
   $($tweetText).on('keydown', function(event) {
-    //console.log( $(this).val().length); //logs the char in textarea
+    // length of input text
     const charLength = $(this).val().length;
+
+    // form itself
     const $textArea = $(this);
+
+    // goes up the DOM tree to find the element matching the selector
     const $tweetForm = $textArea.closest('form');
+
+    // accesses counter
     const $counter = $tweetForm.find('.counter');
     
-    console.log("textArea", $textArea);
-    console.log("tweetForm", $tweetForm);
-    console.log("counter:", $counter);
+    // console.log("textArea", $textArea);
+    // console.log("tweetForm", $tweetForm);
+    // console.log("counter:", $counter);
     $counter.html(140-charLength)
 
+    // color changes in counter
     if (charLength > 140) {
+      // added class to style in CSS if count is over 140
       $counter.addClass("red"); 
     } else {
       $counter.removeClass("red");
     }
     
   })
-  // need to append tweet to initial-tweets.json
-  // 
 });
