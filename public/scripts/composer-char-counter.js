4b19fd604f2a805478b508da65f65f993a82625c
NatalieSokolova <nataliesklv@gmail.com>
2020-07-14 20:55:58 +0000

    linked jQuery and added functionality to change count number

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,8 +1,33 @@
 
-
-
-
-
 $(document).ready(function() {
-  console.log("Successfully loaded!")
+
+  // selector => #tweet-text is my form textarea
+  // section => new-tweet
+  // button = anonymous (in tweet-form-footer)
+  // counter = counter (in tweet-form-footer)
+  // event is change or blur
+  // use .this, make sure it's capturing the right thing
+  const $tweetText = ('#tweet-text');
+  
+  $($tweetText).on('keydown', function(event) {
+    //console.log( $(this).val().length); //logs the char in textarea
+    const charLength = $(this).val().length;
+    const $textArea = $(this);
+    const $tweetForm = $textArea.closest('form');
+    const $counter = $tweetForm.find('.counter');
+    
+    console.log("textArea", $textArea);
+    console.log("tweetForm", $tweetForm);
+    console.log("counter:", $counter);
+    $counter.html(140-charLength)
+
+    if (charLength > 140) {
+      $counter.addClass("red"); 
+    } else {
+      $counter.removeClass("red");
+    }
+    
+  })
+  // need to append tweet to initial-tweets.json
+  // 
 });
