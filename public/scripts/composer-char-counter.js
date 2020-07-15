6187c5c69f00ba8a2476072536ffa81df018686f
NatalieSokolova <nataliesklv@gmail.com>
2020-07-15 01:18:43 +0000

    final touches

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+  const $tweetText = ('#tweet-text');
+  
+  // used keypress as it doesn't react to modifier keys
+  $($tweetText).on('keypress', function(event) {
+    // length of input text
+    const charLength = $(this).val().length;
+
+    // form itself
+    const $textArea = $(this);
+
+    // goes up the DOM tree to find the element matching the selector
+    const $tweetForm = $textArea.closest('form');
+
+    // accesses counter
+    const $counter = $tweetForm.find('.counter');
+    
+    // console.log("textArea", $textArea);
+    // console.log("tweetForm", $tweetForm);
+    // console.log("counter:", $counter);
+    $counter.html(140-charLength)
+
+    // color changes in counter
+    if (charLength > 140) {
+      // added class to style in CSS if count is over 140
+      $counter.addClass("red"); 
+    } else {
+      $counter.removeClass("red");
+    }
+    
+  })
+});
